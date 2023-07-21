8a7e7cd091c91b18ca16fc05dac417fe7567141a
slee <sanlee8495@gmail.com>
2023-07-20 17:36:56 -0700

    7/20/2023

--- R Files/PCS_RPA.R
+++ R Files/PCS_RPA.R
@@ -15,13 +15,22 @@ library(rio)
 ################ Read original files ####################
 
 projectlist <- read_excel("S:/Global Shared Folders/Large Documents/S&OP/PCS/Reporting/Weekly PCS Project Rpt/2023/7 July/ProjectList_7_3_2023 11_44_28 AM.xlsx")
+all_pcs_project_with_mfg_location <- read_csv("S:/Global Shared Folders/Large Documents/S&OP/PCS/Reporting/Stan's folder/All PCS Projects - With MFG Locations (59).csv")
 
-mfg_location <- read_excel("S:/Global Shared Folders/Large Documents/S&OP/PCS/Reporting/PCS Weekly Project Report XMLs/PCS Weekly Report Data MFG Loc XML Revised 2 (Data - MFG Locs).xlsx")
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+##########################################################################################################################################################################
+##########################################################################################################################################################################
 
+
+################ Read Data Fixed files ####################
+active_pack_comp <- read_excel("S:/Global Shared Folders/Large Documents/S&OP/PCS/Reporting/RStudio/Active Pack Comp.xlsx")
 
 ################# ETL (Extract, Transform, Load) ###################
 
-# Project List Data Clean
+##### (Project Coordinator tab)
 projectlist[-1:-5, -1] -> projectlist
 colnames(projectlist) <- projectlist[1, ]
 projectlist[-1, ] -> projectlist
@@ -29,11 +38,21 @@ projectlist[-1, ] -> projectlist
 projectlist %>% 
   janitor::clean_names() %>% 
   dplyr::select(project_id, project_coordinator) %>% 
-  readr::type_convert() -> projectlist
+  readr::type_convert() -> project_coordinator
 
 
-# MFG Location data clean
-mfg_location
+##### (Data - MFG Locs tab)
+all_pcs_project_with_mfg_location %>% 
+  data.frame() %>% 
+  janitor::clean_names()-> data_mfg_locs
 
+data_mfg_locs %>% 
+  dplyr::mutate(dsx_loc = paste0(project_number, pcs_manufacturing_location)) -> data_mfg_locs
 
+active_pack_comp %>% 
+  data.frame() %>% 
+  janitor::clean_names() %>% 
+  dplyr::select(1, 5) %>% 
+  dplyr::rename(dsx_loc = projloc) -> active_pack_comp
 
+active_pack_comp[!duplicated(active_pack_comp[,c("projloc ")]),] -> active_pack_comp
