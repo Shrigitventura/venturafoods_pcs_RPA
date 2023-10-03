47d92d6f3dbd74892697695003c8546c2b4a0831
slee <sanlee8495@gmail.com>
2023-10-03 13:45:35 -0700

    10/3/2023

--- R Files/PCS_RPA_2.R
+++ R Files/PCS_RPA_2.R
@@ -42,7 +42,7 @@ prm <- read_excel("S:/Global Shared Folders/Large Documents/S&OP/PCS/Reporting/R
 ##############################################################################################################################################################
 
 pre_final_product %>% 
-  dplyr::mutate(project_submitted_date = mdy(project_submitted_date)) %>% 
+  dplyr::mutate(project_submitted_date = lubridate::mdy(project_submitted_date)) %>% 
   dplyr::filter(project_submitted_date >= as.Date("2023-09-25") & project_submitted_date <= as.Date("2023-10-01")) %>%  #### Input the past date to figure out new projects ####
   dplyr::select(project_number) -> new_projects
 
